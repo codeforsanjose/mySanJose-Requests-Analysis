413ccf7a0375b2f50338b19a4e76c7b0f6db4b05
Aakash Desai <aakashhdesai@icloud.com>
2018-08-29 15:47:04 -0700

    Request analyses and heatmap

--- mySanJoseRequests.R
+++ mySanJoseRequests.R
@@ -31,7 +31,8 @@ ggplot(mySJ, aes(x=age)) +
 
 # Plot Income across Population in MySJ Requests
 ggplot(mySJ, aes(x=income, y=pop)) +
-  geom_point() +
+  stat_smooth(method="auto") +
+  stat_bin_2d() +
   xlab("Income") +
   ylab("Population") +
   ggtitle("Income across Population in MySJ Requests")
@@ -43,3 +44,49 @@ ggplot(mySJ, aes(x=age, y=income)) +
   xlab("Average Age") +
   ylab("Income") +
   ggtitle("Age across Income in MySJ Requests")
+
+# Requests Analyses
+
+# Shape the dataset
+myAnalysisSJ <- select(source, pop, income, age, Abandoned.Vehicle, Streetlight.Outage, General.Request, Illegal.Dumping, Graffiti, Pothole)
+myAnalysisSJ <- myAnalysisSJ[!is.na(myAnalysisSJ$income),]
+myAnalysisSJ[,'income'] <- round(myAnalysisSJ[,'income'], digits = -2)
+
+# Basic Trend Lines over Income
+ggplot(myAnalysisSJ, aes(x=income)) +
+  stat_smooth(aes(y=Abandoned.Vehicle, colour="Abandoned Vehicle")) +
+  stat_smooth(aes(y=Streetlight.Outage, colour="Streetlight Outage")) +
+  stat_smooth(aes(y=General.Request, colour="General")) +
+  stat_smooth(aes(y=Illegal.Dumping, colour="Illegal Dumpings")) +
+  stat_smooth(aes(y=Graffiti, colour="Graffiti")) +
+  stat_smooth(aes(y=Pothole, colour="Potholes")) +
+  scale_colour_manual(name=c("Abandoned Vehicle","Streetlight Outrage","General","Illegal Dumpings","Graffiti","Potholes"), values=c("red","blue","green","orange","yellow","pink")) +
+  xlab("Income") +
+  ylab("mySJ Requests") +
+  ggtitle("Income across MySJ Requests")
+
+# Basic Trend Lines over Pop
+ggplot(myAnalysisSJ, aes(x=pop)) +
+  stat_smooth(aes(y=Abandoned.Vehicle, colour="Abandoned Vehicle")) +
+  stat_smooth(aes(y=Streetlight.Outage, colour="Streetlight Outage")) +
+  stat_smooth(aes(y=General.Request, colour="General")) +
+  stat_smooth(aes(y=Illegal.Dumping, colour="Illegal Dumpings")) +
+  stat_smooth(aes(y=Graffiti, colour="Graffiti")) +
+  stat_smooth(aes(y=Pothole, colour="Potholes")) +
+  scale_colour_manual(name=c("Abandoned Vehicle","Streetlight Outrage","General","Illegal Dumpings","Graffiti","Potholes"), values=c("red","blue","green","orange","yellow","pink")) +
+  xlab("Population per GeoID") +
+  ylab("mySJ Requests") +
+  ggtitle("Population across MySJ Requests")
+
+# Basic Trend Lines over Age
+ggplot(myAnalysisSJ, aes(x=age)) +
+  stat_smooth(aes(y=Abandoned.Vehicle, colour="Abandoned Vehicle")) +
+  stat_smooth(aes(y=Streetlight.Outage, colour="Streetlight Outage")) +
+  stat_smooth(aes(y=General.Request, colour="General")) +
+  stat_smooth(aes(y=Illegal.Dumping, colour="Illegal Dumpings")) +
+  stat_smooth(aes(y=Graffiti, colour="Graffiti")) +
+  stat_smooth(aes(y=Pothole, colour="Potholes")) +
+  scale_colour_manual(name=c("Abandoned Vehicle","Streetlight Outrage","General","Illegal Dumpings","Graffiti","Potholes"), values=c("red","blue","green","orange","yellow","pink")) +
+  xlab("Average Age") +
+  ylab("mySJ Requests") +
+  ggtitle("Average Age across MySJ Requests")
